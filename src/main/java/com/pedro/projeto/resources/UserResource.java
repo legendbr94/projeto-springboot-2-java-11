9e07e2766821f33bf482d37a020c1117aa5edc7d
Pedro Henrique <phsr94@gmail.com>
2020-10-22 09:53:46 -0300

    User entity and resource

---
+++ src/main/java/com/pedro/projeto/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.pedro.projeto.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pedro.projeto.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Pedro", "teste@gmail", "999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
